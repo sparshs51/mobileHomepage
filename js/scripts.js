1463fd6a484d7c815e90783a6b73500d63d0234d
Sparsh.Singh <=>
2022-02-14 06:25:39 +0530

    modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,51 @@
-/*!
-* Start Bootstrap - Bare v5.0.7 (https://startbootstrap.com/template/bare)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+var tabs = document.getElementById("tabs").children;
+var tabItems = document.getElementById("tab-content").children;
+
+var sidebar = document.getElementById("sidebar");
+var sidebarRight = sidebar.getElementsByClassName("right-side")[0];
+
+function changeTabs(tabName) {
+  if (tabName === "exam") {
+    tabs[0].classList.add("active");
+    tabItems[0].classList.add("active");
+    tabs[1].classList.remove("active");
+    tabItems[1].classList.remove("active");
+  } else {
+    tabs[0].classList.remove("active");
+    tabItems[0].classList.remove("active");
+    tabs[1].classList.add("active");
+    tabItems[1].classList.add("active");
+  }
+}
+
+function toggleSidebar() {
+  sidebar.classList.toggle("hide");
+}
+
+sidebarRight.onclick = toggleSidebar;
+
+// slideshow
+
+const first = document.querySelector(".slide");
+const slide = () => {
+  const before = document.querySelector(".showing");
+  if (before) {
+    before.classList.remove("showing");
+    const next = before.nextElementSibling;
+    if (next) {
+      next.classList.add("showing");
+    } else {
+      first.classList.add("showing");
+    }
+  } else {
+    first.classList.add("showing");
+  }
+};
+slide();
+setInterval(slide, 3000);
+
+// modal
+var modal = document.querySelector("#modal");
+function toggleModal() {
+  modal.classList.toggle("show");
+}
